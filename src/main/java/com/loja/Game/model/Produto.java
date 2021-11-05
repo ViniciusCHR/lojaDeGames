e0f56929b46d10ac44708245027a04a0b6511f05
JMLemos <jlemosdecastro@gmail.com>
2021-11-05 16:07:04 -0300

    @JMLemos [criacao model produto]

---
+++ src/main/java/com/loja/Game/model/Produto.java
@@ -0,0 +1,57 @@
+package com.loja.Game.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idProduto;
+	
+	@NotNull
+	@Size(min = 5 , max = 100)
+	private String nome;
+	
+	@NotNull
+	private double valor;
+	
+	@NotNull
+	private int quantidade;
+	
+	
+	public long getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(long idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+
+}
