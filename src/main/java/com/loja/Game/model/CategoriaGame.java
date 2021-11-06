0dc6724f45bd59316b6a17dc2a1538e254bf0d10
jonatas marques <marquesjon@gmail.com>
2021-11-06 16:44:27 -0300

    @jonatasmarquesb [criando relacionamento onetomany e etc]

--- src/main/java/com/loja/Game/model/CategoriaGame.java
+++ src/main/java/com/loja/Game/model/CategoriaGame.java
@@ -1,12 +1,20 @@
 package com.loja.Game.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity 
 @Table(name= "CategoriaGame")
@@ -31,6 +39,10 @@ public class CategoriaGame {
 	
 	@NotNull
 	private String pagamento;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public String getPagamento() {
 		return pagamento;
